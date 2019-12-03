70c0add2bbae62e458c0d79061ffcf307cbf5320
salahinmushfiq <44909788+salahinmushfiq@users.noreply.github.com>
2019-12-03 15:22:45 +0600

    homePage designed

--- app/src/main/java/com/example/voogle/MainActivity.java
+++ app/src/main/java/com/example/voogle/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.voogle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.voogle.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding activityMainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding=DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+    }
+
+    public void onClickOnGo(View view) {
+
     }
 }
