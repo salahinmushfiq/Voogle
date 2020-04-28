3f2ba94922bb50441d69bd7d310ca1e96e27cf71
Abdullah Zowad Khan <khanabdullahzowad@gmail.com>
2020-04-29 02:47:41 +0600

    getCredentialsById done

--- functions/index.js
+++ functions/index.js
@@ -54,3 +54,60 @@ exports.getCredentials = functions.https.onCall(async request => {
 		}
 	}
 });
+
+exports.getCredentialsById = functions.https.onCall(async request => {
+	try {
+		if (!('phoneNumber' in request)) return {
+			error: "no number provided"
+		}
+		
+		const phoneNumber = request['phoneNumber'];
+		console.log("phoneNumber " + phoneNumber);
+		
+		if (!('groupId' in request)) return {
+			error: "no groupId provided"
+		}
+		
+		const organizationID = request['groupId'];
+		console.log("organization " + organizationID);
+		
+		if (!('licensePlate' in request)) return {
+			error: "no licensePlate provided"
+		}
+		
+		const licensePlate = request['licensePlate'];
+		console.log("licensePlate " + licensePlate);
+		
+		let credential = {email: "error", password: "error"};
+		const dRef = database.ref("root").child("ManagerList")/*.equalTo(organization, "groupName")*/;
+		console.log('dRef ' + dRef);
+		const anything = await dRef.once("value");
+		anything.forEach(child => {
+			console.log(child.val());
+			console.log(child.val()['groupId']);
+			const childX = child.val();
+			if ('phoneNumbers' in childX && childX['groupId'] === organizationID && childX['licensePlate'] === licensePlate) {
+				for (let i = 0; i < childX['phoneNumbers'].length; i++) {
+					console.log(childX['phoneNumbers'][i]);
+					if (childX['phoneNumbers'][i] === phoneNumber) {
+						credential = childX;
+						break;
+					}
+				}
+			}
+		});
+		if (credential['email'] === 'error' || credential['password'] === 'error') return {
+			error: "not found"
+		}
+		console.log(anything);
+		return {
+			email: credential['email'],
+			password: credential['password']
+		}
+	} catch (error) {
+		console.log(error);
+		return {
+			error: error
+		}
+	}
+});
